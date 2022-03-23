717729c99ab8c4a81aa9bac581286e97d329c903
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2022-03-23 18:14:31 -0300

    Update summarizer_I_web_app.py

--- summarizer_I_web_app.py
+++ summarizer_I_web_app.py
@@ -104,7 +104,8 @@ def sumarizador(url, n_sentencas):
     return st.write((HTML(f"""{texto_html}""")))
 
 #Testando algoritmo
-url = st.text_input('__Cole ou digite o link:__')
+st.write('####Cole ou digite o link da página')
+url = st.text_input()
 if st.button('Gerar resumo'):
     sumarizador(url, 2)
 
